30a5b93841aa010f9fd3da1a6a516f5708643ee3
Nico Siirilä <nico.siirila@aalto.fi>
2025-10-22 14:35:35 +0300

    Shader Work

--- player.gd
+++ player.gd
@@ -4,11 +4,10 @@ extends CharacterBody3D
 @export var TURN_SPEED: int = 5
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
-	
-	if Input.is_action_pressed("quit_debug"):
-		get_tree().quit()
+	#if Input.is_action_just_pressed("shoot"):
+	#	shoot()
+	#if Input.is_action_pressed("quit_debug"):
+	#	get_tree().quit()
 	move_player(delta)
 
 func shoot():
